818c1443c660c1d49291124b283879ae12330fe7
chandlerkeyes <ckeyes4@emich.edu>
2017-10-20 12:31:28 -0400

    first commit

---
+++ PigGame.java
@@ -0,0 +1,13 @@
+public class PigGame
+{
+    GVdie d1, d2;
+    int playerScore, computerScore, roundScore;
+    final int WINNING_SCORE = 100;
+    //Contructor
+    public PigGame() {
+        //FIXME: STEP 3 Contructor
+        d1 = new GVdie();
+        d2 = new GVdie();
+    }
+
+}
